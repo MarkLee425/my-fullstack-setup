8ee92a9e93120219363a293bd305459a6a783527
MarkLee425 <leehokwong0425@gmail.com>
2025-09-03 02:49:10 +0800

    feat: add vite csr

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,17 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [tailwindcss(), react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
+	server: {
+		port: 8016,
+	},
+});
